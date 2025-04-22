28ddf7cafc627330749c079b71e65bbb1dc02ba9
leesunuk <leesunook12@naver.com>
2025-04-22 15:41:45 +0900

    3일차 : public, private, static와 setter를 통한 값 변경

---
+++ study/src/main/java/com/study/study/study.java
@@ -0,0 +1,17 @@
+package com.study.study;
+
+public class study {
+
+    private String name;
+    private Integer age;
+
+    public void AgePlus(){
+        this.age = age + 1;
+    }
+
+    public void AgeSet(Integer a){
+        if (100 >= age && age >= 1) {this.age = a;}
+    }
+}
+
+
